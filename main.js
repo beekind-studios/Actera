c8d73a81de2fc802608ddcd69d52beab0affc142
Keaton Hancock <keatonlewishancock@gmail.com>
2020-06-04 19:44:07 -0500

    Implement framless and togglable devtools

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {Menu, MenuItem, app, BrowserWindow} = require('electron')
 const path = require('path')
+
+const menu = new Menu()
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+menu.append(new MenuItem({
+  label: 'Print',
+  accelerator: 'F12',
+  click: () => {
+    mainWindow.webContents.openDevTools()
+  }
+}))
+
+Menu.setApplicationMenu(menu)
+
+// and load the index.html of the app.
+mainWindow.loadFile('index.html')
+  
+// remove window and border on 
+//mainWindow.removeMenu()
+
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
