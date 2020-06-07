8d1a071558fe7f29979727022b33ad874a31dbf1
Justin Carver <5253405+justin-carver@users.noreply.github.com>
2020-06-07 16:57:19 -0500

    Demo Template for Screenplay is GO.

--- renderer.js
+++ renderer.js
@@ -27,13 +27,3 @@ $('#minimize-window').click(function() {
 $('.create-new-project').click(function() {
     ipc.send('create-new-project') 
 })
-
-// Editor functions, can be moved to another js file.
-
-// Allow the user to press tab in the editable divs.
-$('[contenteditable]').on('keydown', function(e){
-    if(e.keyCode == 9){
-        e.preventDefault();
-        document.execCommand('insertHTML', false, '&#009');
-    }
-}).css('white-space', 'pre-wrap');
